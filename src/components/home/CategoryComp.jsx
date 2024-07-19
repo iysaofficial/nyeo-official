2f253f2ed92e2dcdc5b3ccf2aaaf60f4913ab6c7
iysaofficial <youngscientist.iysa@gmail.com>
2024-07-19 14:29:41 +0700

    category delete

--- src/components/home/CategoryComp.jsx
+++ src/components/home/CategoryComp.jsx
@@ -16,7 +16,7 @@ const CategoryComp = () =>{
                     </div>
                 </div>
 
-                <div className="serviceBox">
+                {/* <div className="serviceBox">
                     <div className="icon" style={{"--i":"#0A6847"}}>
                         <img src="./assets/images/icon/University.png" alt="" />
                     </div>
@@ -24,7 +24,7 @@ const CategoryComp = () =>{
                         <h2>Universitas</h2>
                         <p>Bergabunglah dengan olimpiade ekonomi universitas dan tunjukkan kemampuan Anda</p>
                     </div>
-                </div>
+                </div> */}
                 <br />
                 <br />
                 <br />
